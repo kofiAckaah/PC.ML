d7289175b4234ee7b5dc2b14705f91bda89aeb19
Kofi Ackaah <ko.ackaah@yahoo.com>
2024-03-30 05:13:31 +0000

    Added tests.

---
+++ PC.ML.Tests/ServiceTests/PriceCheckerServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+
+using PC.ML.Core.RequestDtos;
+using PC.ML.Core.Services;
+
+namespace PC.ML.Tests.ServiceTests
+{
+    [TestClass]
+    public class PriceCheckerServiceTest
+    {
+        private readonly Mock<IPriceCheckerService> _priceCheckerServiceMock = new();
+        [TestMethod]
+        public async Task CheckPriceTestAsync()
+        {
+            // Arrange
+            var requestDto = new PriceCheckerRequestDto
+            {
+                CPU = "Intel Core i5",
+                GHz = 2.5f,
+                GPU = "Nvidia GTX 1050",
+                RAM = 8f,
+                RAMType = "DDR4",
+                Screen = 15.6f,
+                Storage = 256f,
+                SSD = true,
+                Weight = 2.5f
+            };
+            var priceCheckerService = new PriceCheckerService();
+            // Act
+            var result = await priceCheckerService.CheckPrice(requestDto);
+            // Assert
+            Assert.IsTrue(result > 0m);
+        }
+    }
+}
