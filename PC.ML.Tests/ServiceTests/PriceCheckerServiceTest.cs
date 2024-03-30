cf220a84125660011a9076e6497f8c0f7d344b87
Kofi Ackaah <ko.ackaah@yahoo.com>
2024-03-30 05:23:43 +0000

    Tested methods

--- PC.ML.Tests/ServiceTests/PriceCheckerServiceTest.cs
+++ PC.ML.Tests/ServiceTests/PriceCheckerServiceTest.cs
@@ -1,5 +1,4 @@
-﻿using Moq;
-
+﻿using PC.ML.Core.Exceptions;
 using PC.ML.Core.RequestDtos;
 using PC.ML.Core.Services;
 
@@ -8,7 +7,11 @@ namespace PC.ML.Tests.ServiceTests
     [TestClass]
     public class PriceCheckerServiceTest
     {
-        private readonly Mock<IPriceCheckerService> _priceCheckerServiceMock = new();
+        /// <summary>
+        /// Test for checking the price of a laptop.
+        /// <para>Asserts that the price is greater than 0.</para>
+        /// </summary>
+        /// <returns>Task of the opration.</returns>
         [TestMethod]
         public async Task CheckPriceTestAsync()
         {
@@ -30,6 +33,37 @@ namespace PC.ML.Tests.ServiceTests
             var result = await priceCheckerService.CheckPrice(requestDto);
             // Assert
             Assert.IsTrue(result > 0m);
+        }
+
+        /// <summary>
+        /// Test for checking the price of a laptop if default values are provided.
+        /// <para>Asserts that the price is greater than 0.</para>
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task CheckPriceTestWithDefaltValuesAsync()
+        {
+            // Arrange
+            var priceCheckerService = new PriceCheckerService();
+            // Act
+            var result = await priceCheckerService.CheckPrice(new PriceCheckerRequestDto());
+            // Assert
+            Assert.IsTrue(result > 0m);
+        }
+
+        /// <summary>
+        /// Test for checking the price of a laptop if default values are provided.
+        /// <para>Expect an exception.</para>
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        [ExpectedException(typeof(MLException))]
+        public async Task CheckPriceTestWithNullAsync()
+        {
+            // Arrange
+            var priceCheckerService = new PriceCheckerService();
+            // Act
+            var result = await priceCheckerService.CheckPrice(null);
         }
     }
 }
